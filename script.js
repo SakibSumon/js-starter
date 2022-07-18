718b25df6c46b8dd06fc3594b3b19aca7233744e
SakibSumon <sakib.cse.27@gmail.com>
2022-07-18 12:29:30 +0600

    js first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// // let num = 10;
+// // if (num == 10) {
+// //   document.write("lollllllllllllllllllllllllllllllllllllll");
+// // }
+
+// // console.log(2 + 445 + 782973901 - 9);
+
+// // let pi = 3.1416;
+// // let PI = 3.1416;
+
+// // let country = "bangladesh";
+// // let continent = "Asia";
+// // let population = 150;
+
+// // console.log(
+// //   "country: " +
+// //     country +
+// //     ". Continent: " +
+// //     continent +
+// //     "population: " +
+// //     population +
+// //     "mil"
+// // );
+
+// // let isisland = true;
+// // let language;
+// // console.log(
+// //   typeof isisland +
+// //     " " +
+// //     typeof population +
+// //     " " +
+// //     typeof country +
+// //     " " +
+// //     typeof continent
+// // );
+
+// const language = "bangla";
+// const country = "bangladesh";
+
+// // country = "netherlands";
+// let javascriptIsFun = true;
+
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// const currentTime = 2022;
+
+// const ageSakib = currentTime - 1998;
+// const ageRakib = currentTime - 2002;
+// console.log("Age of Sakib: " + ageSakib + ". Age of Rakib : " + ageRakib);
+
+// console.log(ageSakib / 7, 5 ** 2);
+// let age = Math.sqrt(ageSakib);
+// console.log(age);
+// console.log(Math.sqrt(ageSakib));
+
+// let x = 10 + 5;
+// x += 10;
+// console.log(x);
+
+// console.log(ageSakib < ageRakib);
+// console.log(ageRakib >= 20);
+
+// console.log(currentTime - 1998 > currentTime - 2002);
+
+//coding challenge
+
+let markWeight, markheight, johnWeight, johnHeight;
+
+markWeight = 78;
+markheight = 1.69;
+
+let markBMI = markWeight / markheight ** 2;
+console.log('Mark BMI : '+markBMI);
+
+johnWeight = 92;
+johnHeight = 1.95;
+
+let johnBMI = johnWeight / johnHeight ** 2;
+console.log('John BMI : ' + johnBMI);
+
+let bmiCompare = markBMI > johnBMI;
+
+console.log("mark has greater BMI than John? " + bmiCompare);
+ 
